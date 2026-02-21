c2c8bc7b586051dd78a9daa23f9bec425c35f33b
thedineshjoshi <9868459709dinesh@gmail.com>
2026-02-21 12:40:53 +0545

    Implement AG Grid

--- src/app/Components/layout/sidebar/Citizen/request-service/request-service.component.ts
+++ src/app/Components/layout/sidebar/Citizen/request-service/request-service.component.ts
@@ -1,19 +1,104 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../../../../Services/auth.service';
 import { Router } from '@angular/router';
 import { ApiService } from '../../../../../Services/api.service';
+import { ColDef, ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+import { AgGridAngular } from 'ag-grid-angular';
+ModuleRegistry.registerModules([AllCommunityModule]);
 
+const StatusLabels: Record<number, string> = {
+  1: 'Pending',
+  2: 'Approved',
+  3: 'Rejected',
+};
+const ServiceType:any={
+  1: 'Birth Certificate',
+  2: 'Death Certificate',
+  3: 'Recommendation Letter',
+  4: 'Property Document',
+  5: 'Marriage Registration',
+  6: 'Migration Certificate',
+  7: 'Address Verification'
+}
+const priorityLevel:any={
+  1: 'Normal',
+  2:'Urgent'
+}
 @Component({
   selector: 'app-request-service',
-  imports: [FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, AgGridAngular],
   templateUrl: './request-service.component.html',
   styleUrl: './request-service.component.css'
 })
 export class RequestServiceComponent implements OnInit {
-requestForm!: FormGroup;
+  requestForm!: FormGroup;
+  pageSize=7;
+  public rowData: any[] = [];
+  isBrowser = false;
+
+
+columnDefs:ColDef[]=[
+    {field: 'applicationNumber', headerName: 'Appliction Number', flex: 1, filter: true },
+    {field:'serviceType',headerName:'Service Type',flex:1, filter:true,
+      valueFormatter: (params) => {
+    return ServiceType[params.value] ?? 'Unknown';
+  }
+
+    },
+    {field:'purpose',headerName:'Purpose', flex:1, filter:true},
+    {field:'requestedWard', headerName:'Requested Ward', flex:1, filter:true},
+    {field:'status', headerName:'Status', flex:1, filter:true,
+cellRenderer: (params:any) => {
+    const text = StatusLabels[params.value] ?? 'Unknown';
+    let color = '#666'; // Default Gray
+    
+    if (params.value === 1) color = 'orange'; // Pending
+    if (params.value === 2) color = 'green';  // Approved
+    if (params.value === 3) color = 'red';    // Rejected
+
+    return `<span style="color: ${color}; font-weight: bold;">${text}</span>`;
+  }
+    },
+    {field:'priorityLevel', headerName:'Priority Level', flex:1, filter:true,
+      valueFormatter: (params) => {
+    return priorityLevel[params.value] ?? 'Unknown';
+  }
+    },
+    {field:'createdAt', headerName:'Created At', flex:1, filter:true},
+    {
+      headerName: 'Actions',
+      cellRenderer: (params: any) =>
+      {
+        return `
+        <button class="btn btn-sm btn-outline-primary me-2" data-action="view">
+          <i class="bi bi-pencil"></i> View
+        </button>
+            <button class="btn btn-sm btn-outline-primary me-2" data-action="edit">
+        <i class="bi bi-pencil"></i> Edit
+      </button>
+      <button class="btn btn-sm btn-outline-danger" data-action="delete">
+        <i class="bi bi-trash"></i> Delete
+      </button>
+        `;
+      },
+      onCellClicked: (params: any) => {
+    const action = params.event.target.getAttribute('data-action') || 
+                   params.event.target.parentElement.getAttribute('data-action');
+    
+    if (action === 'edit') {
+      //this.getStaffById(params.data.id);
+    } else if (action === 'delete') {
+      //this.deleteStaff(params.data.id);
+    }
+  }
+    }
+  ]
+
 ngOnInit(): void {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  this.listRequestedServices();
   this.requestForm = this.fb.group({
       ServiceType: ['', Validators.required],
       Purpose: ['', Validators.required],
@@ -75,7 +160,7 @@ serviceOptions = [
     { value: 6, label: 'Address Verification' }
   ];
 
-  constructor(private fb: FormBuilder, private authService:AuthService, private router:Router, private apiService:ApiService) {}
+  constructor(private fb: FormBuilder, private authService:AuthService, private router:Router, private apiService:ApiService,@Inject(PLATFORM_ID) private platformId: Object) {}
 
   get selectedService(): number {
     return Number(this.requestForm.get('ServiceType')?.value);
@@ -119,4 +204,24 @@ serviceOptions = [
       });
     }
 
-}}
+}
+
+listRequestedServices(){
+  const UserId=this.authService.decodeToken().UserId;
+  if(UserId==null)
+  {
+    alert("Login First");
+  }
+  else{
+    this.apiService.getAllService(UserId).subscribe(
+      res=>{
+        this.rowData=res;
+        console.log(res);
+      },
+      err=>{
+        console.log(err);
+      }
+    )
+  }
+}
+}
--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -33,8 +33,8 @@ export class ApiService {
     return this.http.put('https://localhost:7069/api/service-requests/{id}/review',id);
   }
 
-       getService(id: any): Observable<any> {
-    return this.http.get('https://localhost:7069/api/ServiceRequestQuery/my');
+       getAllService(UserId: any): Observable<any> {
+    return this.http.get('https://localhost:7069/api/ServiceRequest/GetAllServices',{params: { userId: UserId }});
   }
        uploadDocument(document: any): Observable<any> {
     return this.http.post('https://localhost:7069/api/DocumentCommand/upload',document);
